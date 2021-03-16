9d37b499c841f75b38753739fc604eb2246bd7d8
yoneji <1>
2021-03-17 04:10:47 +0900

    OBBもうちょっとでできそう。
    texConvを追加。
    先生が作ってくれた、スプライト用のddsConverter

--- GameTemplate/Game/OBB.cpp
+++ GameTemplate/Game/OBB.cpp
@@ -46,3 +46,174 @@ void COBB::Rotating(const Quaternion& rot)
 
 	return;
 }
+
+
+
+
+const bool CollisionOBBs(COBB& obb1, COBB& obb2)
+{
+	///
+	///
+	/// 「分離軸判定」
+	/// 1.OBBの分離軸を探す。
+	///		辺に垂直な分離軸を探す。OBBは直方体のため、辺に垂直なベクトルは
+	///		その辺に隣接している辺を分離軸として使うことができる。（正規化すること）
+	///		つまり、単位方向ベクトルをそのまま分離軸として使う。
+	/// 2.分離軸にOBBを射影する。
+	///		各軸の方向ベクトル（大きさ付き）と分離軸（正規化済み）の内積の絶対値を
+	///		足したものが射影した長さの半分になる。
+	/// 3.二つのOBBの射影線分が重なっているかの判定。
+	///		二つのOBBの中心点間の長さが、
+	///		それぞれの半射影線分の長さの合計より
+	///		長かったら分離している。
+	/// 4.3次元のOBBの場合「双方の方向ベクトルに垂直な分離軸」でも判定する。
+	///		双方のOBBの方向ベクトルの外積で求めることができる。
+	///		
+	///		立体のOBB同士の衝突判定には3+3+9=15本の分離軸を精査する。
+	///		・片方のOBBのそれぞれの単位方向ベクトル	：3本
+	///		・他方のOBBのそれぞれの単位方向ベクトル	：3本
+	///		・双方の方向ベクトルに垂直な分離軸		：3*3=9本
+	/// 
+
+
+
+	//obb1の単位方向ベクトル（正規化済み）
+	Vector3 obb1NDir[COBB::enLocalAxisNum];
+
+	//obb1の単位方向ベクトル（正規化済み）
+	Vector3 obb2NDir[COBB::enLocalAxisNum];
+
+	//obb1の方向ベクトル（大きさ付き）
+	Vector3 obb1DirVec[COBB::enLocalAxisNum];
+
+	//obb2の方向ベクトル（大きさ付き）
+	Vector3 obb2DirVec[COBB::enLocalAxisNum];
+
+
+	//x,y,zの軸をそれぞれ順番に調べて、
+	//大きさ付きの方向ベクトルを確保する。
+	for (int axis = 0; axis < COBB::enLocalAxisNum; axis++)
+	{
+		//obb1の単位方向ベクトルを確保（正規化済み）
+		obb1NDir[axis] = obb1.GetNormalDirection(axis);
+
+		//obb2の単位方向ベクトルを確保（正規化済み）
+		obb2NDir[axis] = obb2.GetNormalDirection(axis);
+
+
+		//obb1の大きさ付の方向ベクトルを計算
+		//単位方向ベクトルに、方向ベクトルの長さを乗算する
+		obb1DirVec[axis] = obb1NDir[axis] * obb1.GetDirectionLength(axis);
+
+		//obb2の大きさ付の方向ベクトルを計算
+		//単位方向ベクトルに、方向ベクトルの長さを乗算する
+		obb2DirVec[axis] = obb2NDir[axis] * obb2.GetDirectionLength(axis);
+	}
+
+
+
+	//obb1とobb2の中心点の間のベクトル
+	Vector3 obb2ToObb1Vec = obb1.GetCenterPosition() - obb2.GetCenterPosition();
+
+	//中心点間の距離
+	float IntervalLen;
+
+	//分離軸
+	Vector3 SeparationAxis;
+
+	//obb1の射影線分
+	float obb1ProjectionLen;
+
+	//obb2の射影線分
+	float obb2ProjectionLen;
+
+
+	//分離軸にobb1の方向ベクトルを使って、
+	//X,Y,Zを順番に精査していく。
+	for (int axis = 0; axis < COBB::enLocalAxisNum; axis++)
+	{
+		//1.OBBの分離軸を探す。
+		//分離軸にobb1のX,Y,Zをそれぞれを選ぶ
+		SeparationAxis = obb1NDir[axis];
+
+		//2.分離軸にOBBを射影する。
+		//obb1の射影線分
+		obb1ProjectionLen = obb1.GetDirectionLength(axis);
+
+		//obb2の射影線分
+		obb2ProjectionLen =
+			CalcProjectionLen(
+				SeparationAxis,
+				obb2DirVec[COBB::enLocalX],
+				obb2DirVec[COBB::enLocalY],
+				obb2DirVec[COBB::enLocalZ]);
+
+		//3.二つのOBBの射影線分が重なっているかの判定。
+
+		//中心点間の距離を計算
+		IntervalLen = std::abs(Dot(obb2ToObb1Vec, SeparationAxis));
+
+		//衝突判定
+		if (IntervalLen > obb1ProjectionLen + obb2ProjectionLen)
+			return false;	//衝突していない
+
+	}
+
+	//分離軸にobb2の方向ベクトルを使って、
+	//X,Y,Zを順番に精査していく。
+	for (int axis = 0; axis < COBB::enLocalAxisNum; axis++)
+	{
+		//1.OBBの分離軸を探す。
+		//分離軸にobb1のX,Y,Zをそれぞれを選ぶ
+		SeparationAxis = obb2NDir[axis];
+
+		//2.分離軸にOBBを射影する。
+		//obb1の射影線分
+		float obb1ProjectionLen =
+			CalcProjectionLen(
+				SeparationAxis,
+				obb1DirVec[COBB::enLocalX],
+				obb1DirVec[COBB::enLocalY],
+				obb1DirVec[COBB::enLocalZ]);
+
+		//obb2の射影線分
+		float obb2ProjectionLen = obb1.GetDirectionLength(axis);
+
+
+		//3.二つのOBBの射影線分が重なっているかの判定。
+
+		//中心点間の距離を計算
+		IntervalLen = std::abs(Dot(obb2ToObb1Vec, SeparationAxis));
+
+		//衝突判定
+		if (IntervalLen > obb1ProjectionLen + obb2ProjectionLen)
+			return false;	//衝突していない
+
+	}
+
+
+	//4.3次元のOBBの場合「双方の方向ベクトルに垂直な分離軸」でも判定する。
+
+	SeparationAxis = Cross(obb1NDir[COBB::enLocalX], obb2NDir[COBB::enLocalX]);
+	obb1ProjectionLen =
+		CalcProjectionLen(SeparationAxis, obb1NDir[COBB::enLocalY], obb1NDir[COBB::enLocalZ]);
+	obb2ProjectionLen =
+		CalcProjectionLen(SeparationAxis, obb2NDir[COBB::enLocalY], obb2NDir[COBB::enLocalZ]);
+	IntervalLen = std::abs(Dot(obb2ToObb1Vec, SeparationAxis));
+
+}
+
+
+const float CalcProjectionLen(
+	const Vector3& sepAxis,
+	const Vector3& dirVec_X,
+	const Vector3& dirVec_Y,
+	const Vector3& dirVec_Z
+)
+{
+	float proj1 = std::abs(Dot(sepAxis, dirVec_X));
+	float proj2 = std::abs(Dot(sepAxis, dirVec_Y));
+	float proj3 = std::abs(Dot(sepAxis, dirVec_Z));
+
+	return proj1 + proj2 + proj3;
+}
--- GameTemplate/Game/OBB.h
+++ GameTemplate/Game/OBB.h
@@ -64,59 +64,11 @@ private:
 };
 
 
+const bool CollisionOBBs(COBB& obb1, COBB& obb2);
 
-bool CollisionOBBs(COBB& obb1, COBB& obb2)
-{
-	//obb1の単位方向ベクトル
-	Vector3 obb1NDir[COBB::enLocalAxisNum];
-	//obb2の単位方向ベクトル
-	Vector3 obb2NDir[COBB::enLocalAxisNum];
-
-	//obb1の方向ベクトル（大きさ付き）
-	Vector3 obb1DirVec[COBB::enLocalAxisNum];
-	//obb2の方向ベクトル（大きさ付き）
-	Vector3 obb2DirVec[COBB::enLocalAxisNum];
-
-
-	//x,y,zの軸をそれぞれ順番に調べて、
-	//単位方向ベクトルと、
-	//大きさ付きの方向ベクトルを確保する。
-	for (int axis = 0; axis < COBB::enLocalAxisNum; axis++)
-	{
-		//obb1の単位方向ベクトルを確保
-		obb1NDir[axis] = obb1.GetNormalDirection(axis);
-
-		//obb2の単位方向ベクトルを確保
-		obb2NDir[axis] = obb2.GetNormalDirection(axis);
-
-
-
-		//obb1の大きさ付の方向ベクトルを計算
-		//単位方向ベクトルに、方向ベクトルの長さを乗算する
-		obb1DirVec[axis] = obb1NDir[axis] * obb1.GetDirectionLength(axis);
-
-		//obb2の大きさ付の方向ベクトルを計算
-		//単位方向ベクトルに、方向ベクトルの長さを乗算する
-		obb2DirVec[axis] = obb2NDir[axis] * obb2.GetDirectionLength(axis);
-	}
-
-
-	//obb1とobb2の中心点の間のベクトル
-	Vector3 intervalVec = obb1.GetCenterPosition() - obb2.GetCenterPosition();
-
-
-
-
-	//分離軸がobb1DirVec[COBB::enLocalX]だとすると
-
-	//obb1の投影線分
-	float obb1ProjectionLen = obb1DirVec[COBB::enLocalX].Length();
-
-
-
-
-
-
-
-
-}
+const float CalcProjectionLen(
+	const Vector3& sepAxis,
+	const Vector3& dirVec_X,
+	const Vector3& dirVec_Y,
+	const Vector3& dirVec_Z = { 0.0f,0.0f,0.0f }
+);
