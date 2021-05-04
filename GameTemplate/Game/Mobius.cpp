e8dac741babce68cd32de115cabcdb2f9aa2551c
wa-i <ie-i>
2021-05-04 21:14:02 +0900

    stage

--- GameTemplate/Game/Mobius.cpp
+++ GameTemplate/Game/Mobius.cpp
@@ -7,7 +7,7 @@ bool Mobius::Start()
 	//モデルレンダラーの生成
 	m_modelRender = NewGO<CModelRender>(0);
 	//モデルレンダラーの初期化
-	m_modelRender->Init("Assets/modelData/test5.tkm");
+	m_modelRender->Init("Assets/modelData/Mobius.tkm");
 	m_modelRender->SetPosition(m_position);
 	m_modelRender->SetRotation(m_rotation);
 
