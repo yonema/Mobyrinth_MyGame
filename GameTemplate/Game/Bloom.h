add5b846f3533b4ebc369931834a29a7edf20386
yoneji <1>
2021-07-20 14:00:55 +0900

    bloomのリファクタリング

--- GameTemplate/Game/Bloom.h
+++ GameTemplate/Game/Bloom.h
@@ -2,6 +2,27 @@
 #include "GaussianBlur.h"
 #include "SpriteRenderConstData.h"
 
+/**
+ * @brief ブルームの定数データ
+*/
+namespace BloomConstData
+{
+	//ガウシアンブラーの数
+	constexpr int GAUSSIAN_BLUR_NUM = 4;
+
+	//ガウシアンブラーの強さ
+	constexpr int POWER_GAUSSIAN_BLUR = 10;
+
+	//ポストエフェクトのシェーダーファイルパス
+	constexpr const char* const SHADER_FILEPATH_POST_EFFECT = "Assets/shader/PostEffect.fx";
+
+	//輝度抽出用スプライト用のピクセルシェーダー
+	constexpr const char* const ENTRY_POINT_FUNC_PS_LUMINANCE_SPRITE = "PSSamplingLuminance";
+
+	//ブルーム用のピクセルシェーダー
+	constexpr const char* const ENTRY_POINT_FUNC_PS_BLOOM = "PSBloomFinal";
+}
+
 class CBloom
 {
 public:		//自動で呼ばれるメンバ関数
@@ -47,7 +68,7 @@ private:	//privateなメンバ関数
 private:	//データメンバ
 	RenderTarget m_luminanceRenderTarget;	//輝度抽出用のレンダリングターゲット
 	Sprite m_luminanceSprite;				//輝度抽出用のスプライト
-	CGaussianBlur m_gaussianBlur[4];		//ガウシアンブラー４枚
+	CGaussianBlur m_gaussianBlur[BloomConstData::GAUSSIAN_BLUR_NUM];	//ガウシアンブラー４枚
 	Sprite m_finalSprite;					//最終的なスプライト
 
 };
